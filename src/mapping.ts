8854c2ac47f3c8535bed6bfc1f05cf94bd0f4118
necofun <necofun.games@gmail.com>
2022-01-18 15:10:39 +0900

    update

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
 import {
   NecoMarketplace,
   BuyItem,
@@ -7,31 +7,38 @@ import {
   PublishNewItem,
   RevertOnListItem
 } from "../generated/NecoMarketplace/NecoMarketplace"
-import { ExampleEntity } from "../generated/schema"
+import {
+  NecoNFT
+} from "../generated/NecoMarketplace/NecoNFT"
+
+import { Marketplace, Item, User } from "../generated/schema"
+
+let ZERO_ADDRESS = Address.fromString("0x0000000000000000000000000000000000000000")
+let BIGINT_ZERO = BigInt.fromI32(0)
 
 export function handleBuyItem(event: BuyItem): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  // let entity = ExampleEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  // if (!entity) {
+  //   entity = new ExampleEntity(event.transaction.from.toHex())
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+  //   // Entity fields can be set using simple assignments
+  //   entity.count = BigInt.fromI32(0)
+  // }
 
   // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // entity.count = entity.count + BigInt.fromI32(1)
 
   // Entity fields can be set based on event parameters
-  entity.buyer = event.params.buyer
-  entity.seller = event.params.seller
+  // entity.buyer = event.params.buyer
+  // entity.seller = event.params.seller
 
   // Entities can be written to the store with `.save()`
-  entity.save()
+  // entity.save()
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -72,6 +79,52 @@ export function handleChangePrice(event: ChangePrice): void {}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 
-export function handlePublishNewItem(event: PublishNewItem): void {}
+export function handlePublishNewItem(event: PublishNewItem): void {
+  let marketAddress = event.address;
+  let marketplaceId = marketAddress.toHex();
+  let marketplace = Marketplace.load(marketplaceId);
+  let marketplaceContract = NecoMarketplace.bind(marketAddress);
+  let nftContract = NecoNFT.bind(marketplaceContract.necoNFT())
+  if (marketplace == null) {
+    marketplace = new Marketplace(marketplaceId);
+    marketplace.publishedItems = [];
+    marketplace.soldItems = [];
+    marketplace.todayPublishedItemAmount = 0;
+    marketplace.todaySoldItemAmount = 0;
+    marketplace.todayPublishedItemAmount = 0;
+    marketplace.todaySoldItemAmount = 0;
+  }
+
+  let userId = event.params.account.toHex()
+  let user = User.load(userId);
+  if (user == null) {
+    user = new User(userId);
+    user.address = event.params.account;
+  }
+
+  let itemId = event.params.ItemId.toHex();
+  let item = Item.load(itemId);
+  if (item == null) {
+    item = new Item(itemId);
+    item.itemId = event.params.ItemId;
+    item.seller = user.id;
+    item.buyer = ZERO_ADDRESS.toHex();
+    item.nftId = event.params.nftId;
+    item.nftUri = nftContract.uri(item.nftId);
+    item.amount = event.params.amount;
+    item.price = event.params.price;
+    item.onListTime = event.block.timestamp;
+    item.isSoldOut = false;
+    item.isOnList = true;
+    item.fee = BIGINT_ZERO;
+    item.save();
+  }
+
+  marketplace.publishedItems.push(item.id);
+  marketplace.save();
+
+  user.onListItems.push(item.id);
+  user.save()
+}
 
 export function handleRevertOnListItem(event: RevertOnListItem): void {}
